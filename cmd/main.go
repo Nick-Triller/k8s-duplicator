3a6b2f28e3537bafa3056a4c643cb615d9e3cf66
Nick Triller <nicktriller@gmail.com>
2023-12-15 09:37:24 +0100

    Scaffold secret controller
    kubebuilder create api --group "core.k8s.io" --kind "Secret" --version "v1" --controller --resource=false

--- cmd/main.go
+++ cmd/main.go
@@ -31,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	"github.com/Nick-Triller/k8s-duplicator/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -85,6 +87,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.SecretReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Secret")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
